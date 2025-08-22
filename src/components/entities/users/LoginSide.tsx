ba16db16d6979486cf923dadc2b53a3b28e510c2
Martín Gesualdo <mgesualdo14@gmail.com>
2025-08-22 13:21:19 -0300

    Cambios

--- src/components/entities/users/LoginSide.tsx
+++ src/components/entities/users/LoginSide.tsx
@@ -1,30 +1,27 @@
 import LoginForm from "./LoginForm";
-import TutorialBadge from "@/components/custom-ui/TutorialBadge";
 import Image from "next/image";
 import { Suspense } from "react";
-import TutorialVideo from "../tutorials/TutorialVideo";
 
 const LoginSide = ({ ownersPage = false }) => {
   return (
     <div className="flex items-start  justify-center  pb-6 pt-2 sm:pt-10 lg:pb-24 bg-white h-screen">
-      <div className="flex flex-col items-center justify-center w-[350px] gap-6">
+      <div className="flex flex-col items-center justify-center w-[280px] gap-6">
         <div
           className={`flex items-center gap-2 ${ownersPage ? "pb-3" : "pb-6"} -mb-6 sm:-mb-3`}
         >
           <Image
             src="/logo.png"
-            className="rounded-full w-11 sm:w-16"
+            className="rounded-full w-40"
             alt="Image"
             width={90}
             height={60}
           />
-          <h1 className="text-gray-800 text-4xl   sm:text-6xl  font-bold">
-            Caserito
-          </h1>
         </div>
 
         <div className="flex flex-col items-center gap-2 text-center">
-          <h1 className="text-2xl lg:text-3xl font-bold">Iniciar sesión</h1>
+          <h1 className="text-2xl lg:text-3xl font-bold mt-4">
+            Iniciar sesión
+          </h1>
         </div>
         <div className="grid gap-4 w-full">
           <div className="grid gap-2">
