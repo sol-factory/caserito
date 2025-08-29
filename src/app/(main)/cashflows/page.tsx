cc9779ca504e6bf7794376fb635e8e87dbe3b5fc
Martín Gesualdo <mgesualdo14@gmail.com>
2025-08-29 09:55:21 -0300

    Cambios

--- src/app/(main)/cashflows/page.tsx
+++ src/app/(main)/cashflows/page.tsx
@@ -347,9 +347,11 @@ export default async function Cashflows({ searchParams }) {
         />
 
         <WalletsSummary
-          gatheredByWallet={gatheredByWallet.filter(
-            (w) => w._id !== "68b070046e14411ac916d3c7"
-          )}
+          gatheredByWallet={
+            gatheredByWallet?.filter(
+              (w) => w._id !== "68b070046e14411ac916d3c7"
+            ) || []
+          }
           closures={[]}
           date={dateToFilter}
           dayFilters={dayFilters}
